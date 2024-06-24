f14ae2a552a4ebfa02023c9501c11e78a11775b3
Will Moran <will.c.moran@gmail.com>
2024-06-24 15:12:54 -0400

    Fixed league leaderboard routing

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,17 +1,9 @@
-import { useEffect, useState } from "react"
 import { AdministratorViews } from "./AdministratorViews.jsx"
 import { CompetitorViews } from "./CompetitorViews.jsx"
 
 
-export const ApplicationViews = () => {
-  const [currentUser, setCurrentUser] = useState({})
+export const ApplicationViews = ({ currentUser }) => {
 
-  useEffect(() => {
-    const localVertiUser = localStorage.getItem("verti_user")
-    const vertiUserObject = JSON.parse(localVertiUser)
 
-    setCurrentUser(vertiUserObject)
-  }, [])
-
-  return currentUser.isStaff ? <AdministratorViews currentUser={currentUser} /> : <CompetitorViews currentUser={currentUser}/>
+  return currentUser?.isStaff ? <AdministratorViews currentUser={currentUser} /> : <CompetitorViews currentUser={currentUser}/>
 }
