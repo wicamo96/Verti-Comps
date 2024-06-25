51eff00164476324c6d824c476bd3d60ac9af0e5
Will Moran <will.c.moran@gmail.com>
2024-06-25 15:06:48 -0400

    Added dashboard feature'

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react"
 import { AdministratorViews } from "./AdministratorViews.jsx"
 import { CompetitorViews } from "./CompetitorViews.jsx"
 
 
-export const ApplicationViews = ({ currentUser }) => {
+export const ApplicationViews = () => {
+  const [currentUser, setCurrentUser] = useState({})
 
+  useEffect(() => {
+    const localVertiUser = localStorage.getItem("verti_user")
+    const vertiUserObject = JSON.parse(localVertiUser)
 
-  return currentUser?.isStaff ? <AdministratorViews currentUser={currentUser} /> : <CompetitorViews currentUser={currentUser}/>
+    setCurrentUser(vertiUserObject)
+  }, [])
+
+  return currentUser.isStaff ? <AdministratorViews currentUser={currentUser} /> : <CompetitorViews currentUser={currentUser}/>
 }
